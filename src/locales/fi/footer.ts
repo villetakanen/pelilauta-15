ee19a8c992b59641449fbe28131e42126a764ea0
Ville Takanen <vitkukissa@gmail.com>
2024-05-28 13:38:57 +0300

    fix: footer ready for beta

--- src/locales/fi/footer.ts
+++ src/locales/fi/footer.ts
@@ -5,5 +5,22 @@ export const footer: TranslationKey = {
     title: 'Pelilauta / Mekanismi',
     description: "Haluttaisiin, että oisi sellainen foorumi. Siis niinkun somen ja chattien lisäksi. Ois kiva kirjoitella ja lukea asioista myös vähän verkkaisemmassa mediassa. Ja ei haittaisi, jos siihen sais yhdistettyä pelien dokumentoinnin ja materiaalin jakamisen Mekanismin tyyliin.",
     linkAbout: 'Lisätietoja sovelluksesta',
+  },
+  affiliates: {
+    title: 'Muut sivustot',
+    links: {
+      myrrys: {
+        title: "Myrrys",
+        description: "Legendoja &  Lohikääreitä kustanaja Myrrys", 
+      }, 
+      loki: {
+        title: "Loki",
+        description: "Roolipeliblogi harrastajilta harrastajille.",
+      },
+      roolipelitiedotus: {
+        title: "Roolipelitiedotus",
+        description: "Roolipelitiedotus on roolipelaamisen ja -pelien tiedotuskanava.",
+      },
+    },
   }
 }
